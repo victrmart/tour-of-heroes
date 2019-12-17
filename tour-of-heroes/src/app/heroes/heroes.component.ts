4b8d7efccc42689e528abdc27533bd2bcd29da9f
Victor Tinoco - VTN <victormartinstinoco@live.com>
2019-12-17 15:48:36 -0300

    Módulo 2 do passo a passo

--- tour-of-heroes/src/app/heroes/heroes.component.ts
+++ tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,17 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
+import { HEROES } from "../mock-heroes";
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.css']
+  styleUrls: ['./heroes.component.css', '../app.component.css']
 })
+
 export class HeroesComponent implements OnInit {
-  hero: Hero = new Hero(1, 'Windstorm');
+  heroes: Hero[] = HEROES;
+  selectedHero: Hero;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onSelect(hero: Hero){
+    this.selectedHero = hero;
   }
 
 }
