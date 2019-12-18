dd68d4d5eb041e89eb62996673f9fc4fdd24d46a
Victor Tinoco - VTN <victormartinstinoco@live.com>
2019-12-18 10:22:28 -0300

    Adicionando async req de heroes por services (parte 1 da seção 4)

--- tour-of-heroes/src/app/heroes/heroes.component.ts
+++ tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from "../mock-heroes";
+import { HeroService } from '../hero.service';
+import { ThrowStmt } from '@angular/compiler';
 
 @Component({
   selector: 'app-heroes',
@@ -9,16 +10,21 @@ import { HEROES } from "../mock-heroes";
 })
 
 export class HeroesComponent implements OnInit {
-  heroes: Hero[] = HEROES;
+  heroes: Hero[];
   selectedHero: Hero;
 
-  constructor() { }
+  constructor(private heroService: HeroService) { }
 
   ngOnInit() {
+    this.getHeroes();
   }
 
   onSelect(hero: Hero){
     this.selectedHero = hero;
   }
 
+  getHeroes(): void{
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes)
+  }
 }
